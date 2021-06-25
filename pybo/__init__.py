dbf135a87b0d360f1e3821156c3eb715bcb40cac
Yongjoo Lee <yongjool@qti.qualcomm.com>
2021-06-25 15:45:59 +0900

    어플리케이션 팩토리, app 객체 생성하기

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
-app = Flask(__name__)
 
-@app.route('/')
-def hello_pybo():
-    return 'Hello, Pybo!'
+
+# 애플리케이션 팩토리는 쉽게 말해 app 객체를 생성하는 함수를 의미한다.
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello_pybo():
+        return 'Hello, Pybo!'
+
+    return app
